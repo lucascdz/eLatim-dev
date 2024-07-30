a2899a919db4aed9b9a70a0aa4bd3677022cc894
ligeialugli <ligeia.lugli@kcl.ac.uk>
2024-07-30 19:16:16 +0200

    revised deployment script

--- ToDeploy.R
+++ ToDeploy.R
@@ -1,16 +1,15 @@
 
-# to deploy this app to shinyapps.io you need to raise the number of files allowed by rsconnect
-# uncomment the following to do so:
-
-
-
-#options(rsconnect.max.bundle.files=length(dir("."))+length(dir("./data"))+length(dir("./Media"))+length(dir("./AltText"))+20)
-
-# then edit the bits in caps and run the deployment command below:
-
-#rsconnect::deployApp(appName="SHORT_DICT_NAME_WITHOUT_SPACES",appTitle="YOUR_DICT_NAME" , account="YOUR_SHINYAPPS_ACCOUNT_NAME")
-
-#example DO NOT RUN:
-#rsconnect::deployApp(appName="DiachronicTibetanVerbValencyDictionary",appTitle="Diachronic dictionary of Tibetan verb valency" , account="mangalamresearch")
+# # to deploy this app to shinyapps.io you need to raise the number of files allowed by rsconnect
+# # uncomment the following to do so:
+# 
+# 
+# options(rsconnect.max.bundle.files=length(list.files("./", full.names = TRUE, recursive = TRUE))+10)
+# 
+# # then edit the bits in caps and run the deployment command below:
+# 
+# rsconnect::deployApp(appName="SHORT_DICT_NAME_WITHOUT_SPACES",appTitle="YOUR_DICT_NAME" , account="YOUR_SHINYAPPS_ACCOUNT_NAME")
+# 
+# # example DO NOT RUN:
+# # rsconnect::deployApp(appName="DiachronicTibetanVerbValencyDictionary",appTitle="Diachronic dictionary of Tibetan verb valency" , account="mangalamresearch")
 
 
