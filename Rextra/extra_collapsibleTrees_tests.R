53bd1bb2423331bee4a914677e6264b2cadb74ff
Lucas Dezotti <lucascdz@gmail.com>
2025-03-08 13:59:10 -0300

    Make Trees

--- Rextra/extra_collapsibleTrees_tests.R
+++ Rextra/extra_collapsibleTrees_tests.R
@@ -1,47 +1,56 @@
-TreeDF <- read.csv('/Users/lucascdz/Rprojects/iLexicographR/data/LexicalData_top999/lexidata_wordnetLexemes.csv')
-
-TreeList <- split(TreeDF,TreeDF$lemma)
-
-collapsibleTree(TreeList[['appello']],
-                hierarchy = c('sem_type','definition_pt','lexemes_pt'),
-                root = names(TreeList['appello']),
-                #attribute = "n_lex",
-                #nodeSize = "hits",
-                fontSize= 15,
-                #tooltip=T,
-                collapsed = T,
-                width = 800,
-                height = 600,
-                zoomable = T
-)
-
-
-### EXAMPLE IN COLORS
-
-species <- read.csv("https://apps.fs.usda.gov/fia/datamart/CSV/REF_SPECIES_GROUP.csv")
-
-collapsibleTree(
-   species,
-   hierarchy = c("REGION", "CLASS", "NAME"),
-   fill = c(
-      # The root
-      "seashell",
-      # Unique regions
-      rep("brown", length(unique(species$REGION))),
-      # Unique classes per region
-      rep("khaki", length(unique(paste(species$REGION, species$CLASS)))),
-      # Unique names per region
-      rep("forestgreen", length(unique(paste(species$NAME, species$REGION))))
-   )
-)
-
-
-
-### EXAMPLE
-parent <- c(NA, "Carlos", "Carlos", "María", "María", "Paula", "Alex")
-child <- c("Carlos", "María", "Alex", "Javier", "Paula", "Pablo", "Pepe")
-
-df <- data.frame(parent, child)
-
+library(tidyverse)
 library(collapsibleTree)
-collapsibleTreeNetwork(df, collapsed = FALSE)
+library(htmlwidgets)
+
+# functions
+SafeEncodeUTF8 <- function(string){
+   paste(utf8ToInt(string), collapse = "-")
+}
+MakeSafeForFilename <- function(string){
+   paste0("_xx",SafeEncodeUTF8(string), "xx_")
+}
+MakeTree <- function(TreeDF, HierarchicalValsVec, rootVal, TargetDir=NULL, Interactive="interactive"){
+
+   Tree <-  collapsibleTree(TreeDF,
+                            hierarchy = c(as.character(HierarchicalValsVec)),
+                            root =  as.character(rootVal), #
+                            #attribute = "n_lex",
+                            #nodeSize = "hits",
+                            fontSize= 20,
+                            #tooltip=T,
+                            collapsed = T,
+                            width = 800,
+                            height = 600,
+                            zoomable = T
+   )
+
+   if(!is.null(TargetDir)){
+      if(Interactive=="interactive"){
+         saveWidget(Tree,paste0(TargetDir,"/Tree_",MakeSafeForFilename(rootVal),"_",paste(HierarchicalValsVec,collapse = "_"),".html"), selfcontained = T)
+      }else{
+         saveWidget(Tree,"tmp.html",selfcontained = F)
+         webshot("tmp.html",paste0(TargetDir,"/Tree_",MakeSafeForFilename(rootVal),"_BY_",paste(HierarchicalValsVec,collapse = "_"),".png"), delay =5, vwidth = 480, vheight=480) # changed to png.
+      }
+   }else{
+      Tree
+   }
+
+}
+
+# arguments
+WordnetDF <- read.csv('/Users/lucascdz/Rprojects/iLexicographR/data/LexicalData_top999/lexidata_wordnetLexemes.csv')
+WordnetList <- split(WordnetDF,WordnetDF$lemma)
+
+# test
+{ print(names(WordnetList[545])) ;
+   MakeTree(TreeDF = WordnetList[[545]],
+            HierarchicalValsVec = c('sem_type','lexemes_pt'),
+            rootVal = names(WordnetList[545]),
+            TargetDir = './data/Plots') }
+
+# make trees for all lemmas
+lapply(seq_along(WordnetList), function(i) {
+   print(names(WordnetList[i])) ; MakeTree(TreeDF = WordnetList[[i]],
+                                           HierarchicalValsVec = c('sem_type','lexemes_pt'),
+                                           rootVal = names(WordnetList[i]),
+                                           TargetDir = './data/Plots') } )
